493471097c07f5f60e13855bd76d1ce4e3bd414c
iArt <iart@aboutdev.io>
2022-12-02 22:59:36 +0700

    Switch logger mode by NODE_ENV :tada:

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,8 @@ import createError, { HttpError } from 'http-errors'
 import { createPDFRouter } from './routers/new'
 
 const app = express()
-
 app.use(cors())
-app.use(logger('dev'))
+app.use(logger(process.env.NODE_ENV == 'development' ? 'dev' : 'combined'))
 app.use(express.json())
 
 app.use('/api/pdf', createPDFRouter)
