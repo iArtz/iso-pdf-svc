9972cb98b53850d815b5d05b3a61bdb13abb885d
iArt <iart@aboutdev.io>
2022-12-02 17:41:23 +0700

    Remove dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv'
 import { app } from './app'
 
 const start = () => {
-    dotenv.config()
-
     if (!process.env.NODE_ENV) throw new Error('NODE_ENV must be defined')
 
     const PORT = process.env.PORT || 3000
